d99d05ddf7141cce903dc1bdc38f06a5848dc34d
Danilo <daniloaugusto0212@hotmail.com>
2021-08-23 21:08:11 -0300

    Formatando número da barra de pesquisa

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,20 @@
-$(function(){
+$(function () {
 
     var currentValue = 0
     var isDrag = false
     var precoMaximo = 70000
     var precoAtual = 0
 
-    $('.pointer-barra').mousedown(function(){
+    $('.pointer-barra').mousedown(function () {
         isDrag = true
     })
 
-    $(document).mouseup(function(){
+    $(document).mouseup(function () {
         isDrag = false
         desableTextSelection()
     })
 
-    $('.barra-preco').mousemove(function(e){
+    $('.barra-preco').mousemove(function (e) {
         if (isDrag) {
             desableTextSelection()
             var elBase = $(this)
@@ -25,21 +25,40 @@ $(function(){
                 mouseX = elBase.width()
             }
 
-            $('.pointer-barra').css('left', (mouseX-13)+'px')
+            $('.pointer-barra').css('left', (mouseX - 13) + 'px')
             currentValue = (mouseX / elBase.width()) * 100
-            $('.barra-preco-fill').css('width', currentValue+'%')
+            $('.barra-preco-fill').css('width', currentValue + '%')
 
-            //TODO: Ajustar o formato do preço.
             precoAtual = currentValue / 100 * precoMaximo
+            precoAtual = formatarPreco(precoAtual)
             $('.preco_pesquisa').html('R$' + precoAtual)
         }
     })
 
-    function desableTextSelection(){
-        $('body').css('-webkit-user-select','none')
-        $('body').css('-moz-user-select','none')
-        $('body').css('-ms-user-select','none')
-        $('body').css('-o-user-select','none')
-        $('body').css('user-select','none')
+    function formatarPreco(precoAtual) {
+        precoAtual = precoAtual.toFixed(2)
+        precoArr = precoAtual.split('.')
+        
+        var novoPreco = formatarTotal(precoArr)
+
+        return novoPreco
+    }
+
+    function formatarTotal(precoArr) {
+        if (precoArr[0] < 1000) {
+            return precoArr[0] + ',' + precoArr[1]
+        } else if (precoArr[0] < 10000) {
+            return precoArr[0][0] + '.' + precoArr[0].substr(1,precoArr[0].length) + ',' + precoArr[1]
+        } else {
+            return precoArr[0][0] + precoArr[0][1] + '.' + precoArr[0].substr(2,precoArr[0].length) + ',' + precoArr[1]
+        }
+    }
+
+    function desableTextSelection() {
+        $('body').css('-webkit-user-select', 'none')
+        $('body').css('-moz-user-select', 'none')
+        $('body').css('-ms-user-select', 'none')
+        $('body').css('-o-user-select', 'none')
+        $('body').css('user-select', 'none')
     }
 })
